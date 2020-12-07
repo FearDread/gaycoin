4b8033cf16ef5649dc1259d61419a96ea10030c0
FearDread <ghaptonstall@gmail.com>
2020-12-07 15:10:02 -0600

    --add src/classes

---
+++ src/classes/GayBlock.js
@@ -0,0 +1,35 @@
+
+const SHA256 = require("crypto-js/sha256");
+
+class CryptoBlock {
+
+	constructor (index, timestamp, data, precedingHash = " ") {
+
+		this.index = index;
+		this.timestamp = timestamp;
+		this.data = data;
+		this.precedingHash = precedingHash;
+		this.hash = this.ComputeHash();
+		this.nonce = 0;
+	}
+
+	ComputeHash () {
+
+		return SHA256 (
+			this.index +
+			this.precedingHash +
+			this.timestamp + JSON.stringify(this.data) +
+			this.nonce
+		).toString();
+	}
+
+	ProofOfWork (difficulty) {
+
+		while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+
+			this.nonce++;
+			this.hash = this.ComputeHash();
+		}
+	}
+}
+
---
+++ src/classes/GayBlockchain.js
@@ -0,0 +1,49 @@
+
+class GayBlockchain {
+
+	constructor () {
+
+		this.blockchain = [this.startGenesisBlock()];
+		this.difficulty = 4;
+	}
+
+	GenesisBlock () {
+
+		return new CryptoBlock(0, "01/01/2020", "Initial Block in the Chain", "0");
+	}
+
+	GetLatestBlock () {
+
+		return this.blockchain[this.blockchain.length - 1];
+	}
+
+	AddNewBlock (block) {
+
+		block.precedingHash = this.obtainLatestBlock().hash;
+		block.proofOfWork(this.difficulty);
+
+		//block.hash = newBlock.computeHash();
+
+		this.blockchain.push(newBlock);
+	}
+
+	CheckChainValidity () {
+
+		for (let i = 1; i < this.blockchain.length; i++) {
+
+			const currentBlock = this.blockchain[i];
+			const precedingBlock = this.blockchain[i - 1];
+
+			if (currentBlock.hash !== currentBlock.computeHash()) {
+				return false;
+			}
+
+			if (currentBlock.precedingHash !== precedingBlock.hash) return false;
+		}
+
+		return true;
+	}
+}
+
+module.exports = GayBlockchain;
+
