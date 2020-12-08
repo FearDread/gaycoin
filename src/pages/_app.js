d862ddbe1e043a68039201d27408cf6c3fb5d608
FearDread <ghaptonstall@gmail.com>
2020-12-08 06:57:35 -0600

    --add custom _app.js for loading custom assets

---
+++ src/pages/_app.js
@@ -0,0 +1,12 @@
+
+import '../stylesheet-example.css'
+
+
+
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp ({ Component, pageProps }) {
+
+	return <Component {...pageProps} />
+}
+
