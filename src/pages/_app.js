7e9575f491caa4360c027b5ae06363f26aec7be2
FearDread <ghaptonstall@gmail.com>
2020-12-09 13:01:01 -0600

    --move needed resources and update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 
-import '../stylesheet-example.css'
+import '../../public/assets/bootstrap/css/bootstrap.min.css'
+
+
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp ({ Component, pageProps }) {
