e0ac2cc686f6cdf8686d38d6942f8519c749df0c
FearDread <ghaptonstall@gmail.com>
2020-12-09 13:18:24 -0600

    --attempt to add Loader HTML via dom

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 
 import '../../public/assets/bootstrap/css/bootstrap.min.css'
 
-
-
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp ({ Component, pageProps }) {
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
 
+function Loader () {
+  
+	return 
+		<div id="loader-wrapper">
+			<div id="loading-center-absolute">
+				<div class="object" id="object_four"></div>
+				<div class="object" id="object_three"></div>
+				<div class="object" id="object_two"></div>
+				<div class="object" id="object_one"></div>
+			</div>
+
+			<div class="loader-section section-left"></div>
+			<div class="loader-section section-right"></div>
+		</div>;
+}
+
+
 function HomePage () {
+
+	Loader();
+
 	
 	return <div>Meet the first Gay Crypto ...... GAYCOIN</div>
 
