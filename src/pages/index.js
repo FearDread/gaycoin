2e09168f02cfcd3336cf0407ec72c525c791168f
FearDread <ghaptonstall@gmail.com>
2020-12-10 10:09:47 -0600

    --testing next js logic in main index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,22 @@
 
 function Loader () {
-  
-	return 
-		<div id="loader-wrapper">
-			<div id="loading-center-absolute">
-				<div class="object" id="object_four"></div>
-				<div class="object" id="object_three"></div>
-				<div class="object" id="object_two"></div>
-				<div class="object" id="object_one"></div>
-			</div>
-
-			<div class="loader-section section-left"></div>
-			<div class="loader-section section-right"></div>
-		</div>;
 }
 
 
 function HomePage () {
-
-	Loader();
-
 	
-	return <div>Meet the first Gay Crypto ...... GAYCOIN</div>
+	return ( <div id="loader-wrapper">
+			<div id="loading-center-absolute">
+				<div className="object" id="object_four"></div>
+				<div className="object" id="object_three"></div>
+				<div className="object" id="object_two"></div>
+				<div className="object" id="object_one"></div>
+			</div>
 
+			<div className="loader-section section-left"></div>
+			<div className="loader-section section-right"></div>
+			</div>
+	)
 }
 
 
